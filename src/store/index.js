b7e1420d11677bb7f9b191ed495ea8dd86279e0d
ZhenyaGro <bon4ios@gmail.com>
2023-10-10 22:32:58 +0300

    Main page data

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,12 @@ import { createStore } from 'vuex';
 export default createStore({
   state() {
     return {
-      logoPath: './imgs/logo.png'
+      logoPath: './imgs/logo.png',
+      boxesPath: './imgs/boxes.png',
+      heading: {
+        head: 'КОЛЛЕКЦИОННЫЕ ИЗДАНИЯ',
+        text: 'Побеждайте легко, побеждайте стильно с новыми коллекционными изданиями «Аллодов Онлайн»!'
+      }
     }
   },
   getters: {
