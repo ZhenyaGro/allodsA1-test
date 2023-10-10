b3f1f80a493969a918853cb2658d06a14cf9de96
ZhenyaGro <bon4ios@gmail.com>
2023-10-10 22:01:20 +0300

    Add backgdound

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-  </div>
+  <main class="main">
+    <div class="background__bottom"></div>
+  </main>
 </template>
 
 <script>
@@ -12,4 +13,16 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.main {
+  min-height: 892px;
+  background-image:
+    url(/public/imgs/Layer-157-1.png),
+    url(/public/imgs/Layer-157-2.png),
+    url(/public/imgs/Layer-157-3.png),
+    url(/public/imgs/Hue_Saturation5-1.jpg);
+  background-repeat: repeat-y, repeat-y, no-repeat, no-repeat;
+  background-size: contain, contain, cover, cover;
+  background-position: left, right, bottom, center;
+}
+</style>
