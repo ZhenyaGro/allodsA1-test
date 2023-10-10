3c67948f9b0363966eab5c1283b10c3c20f4da8f
ZhenyaGro <bon4ios@gmail.com>
2023-10-10 23:13:07 +0300

    Add glow. Main page is done

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -33,16 +33,20 @@ export default {
 .main {
   padding-bottom: 47px;
   background-image:
+    url(/public/imgs/glow1.png),
     url(/public/imgs/Layer-157-1.png),
     url(/public/imgs/Layer-157-2.png),
+    url(/public/imgs/blue-glow3.png),
     url(/public/imgs/Layer-157-3.png),
     url(/public/imgs/MafiaCostume_2022-2.png),
     url(/public/imgs/MafiaCostume_2022-3.png),
     url(/public/imgs/Hue_Saturation5-1.jpg);
-  background-repeat: repeat-y, repeat-y, no-repeat, no-repeat, no-repeat, no-repeat;
-  background-size: auto, auto, contain, auto, auto, cover;
-  background-position: left, right, bottom, bottom 0 left, bottom 0 right, center;
+  background-repeat: no-repeat, repeat-y, repeat-y, no-repeat, no-repeat, no-repeat, no-repeat, no-repeat;
+  background-size: cover, auto, auto, contain, contain, auto, auto, cover;
+  background-position: center, left, right, bottom, bottom, bottom 0 left, bottom 0 right, center;
+  background-blend-mode: screen, normal, normal, normal, normal, normal, normal, normal;
 }
+
 
 .pictures {
   display: flex;
