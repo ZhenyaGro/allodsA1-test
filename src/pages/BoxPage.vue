4347810789bee6041f1eda69f8a947516df75317
ZhenyaGro <bon4ios@gmail.com>
2023-10-10 23:39:01 +0300

    BoxPage draft

---
+++ src/pages/BoxPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box-page">
+    <!-- 
+      BoxPicture
+      BoxContents -> includes BoxContent
+      BoxView
+     -->
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.box-page {
+  background-image: url(/public/imgs/background-wall.jpg);
+  height: 1024px;
+  width: 1000px;
+}
+</style>
