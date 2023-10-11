cffc5b6b3745985e1ea2457de9c8e606f89bc9d0
ZhenyaGro <bon4ios@gmail.com>
2023-10-11 22:04:40 +0300

    Component in progress

---
+++ src/components/BoxMain.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img :src="imgPath" alt="главное в наборе">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BoxMain',
+  props: ['imgPath']
+}
+</script>
+
+<style lang="scss" scoped>
+.test {
+  border-radius: 686px;
+  border: 1px solid #000;
+  opacity: 0.2;
+  background: #416DF4;
+  mix-blend-mode: lighten;
+  filter: blur(100px);
+}
+</style>
